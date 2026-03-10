5b6a7578c573b7cc179b1e1386d5b5c36f711124
faiz-ulurrasyadi <faizulurrasyadi04@gmail.com>
2026-03-10 22:04:05 +0700

    Add Products component with styling for layout and filter box

---
+++ src/components/Products.jsx
@@ -0,0 +1,16 @@
+import './Products.css'
+
+function Products() {
+    return (
+        <div className="products-component">
+            <div className="filter-box">
+                <h2>Filter box</h2>
+            </div>
+            <div className="products-container">
+                <h2>Products box</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Products
