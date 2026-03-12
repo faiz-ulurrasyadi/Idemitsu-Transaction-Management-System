f364dfbb1227fd811e44a7c4efd424610e64f0ed
faiz-ulurrasyadi <faizulurrasyadi04@gmail.com>
2026-03-12 21:09:03 +0700

    Add ListProducts component with product fetching and styling; update MyPages CSS for improved content padding

--- src/components/ListProducts.jsx
+++ src/components/ListProducts.jsx
@@ -1,6 +1,35 @@
+import { supabase } from '../services/supabase-client'
+import { useState, useEffect } from 'react'
+import './ListProducts.css'
+
 function ListProducts(){
+    const [products, setProducts] = useState([])
+    const fetchData = async () => {
+        const {error, data} = await supabase.from('products_oil').select('*').order('id', {ascending: true})
+        setProducts(data)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
     return (
-        <h1>List of products page</h1>
+        <>
+            <div className="product-header">
+                <p>Product name</p>
+                <p>Vehicle type</p>
+                <p>Grade</p>
+                <p>Transmission</p>
+            </div>
+            {products.map((product, idx) => (
+                <div className="product-container">
+                    <p>{product.name}</p>
+                    <p>{product.vec_type}</p>
+                    <p>{product.quality}</p>
+                    <p>{product.transmission}</p>
+                </div>
+            ))}
+        </>
     )
 }
 
